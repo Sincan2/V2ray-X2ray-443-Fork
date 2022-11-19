55b949a37110a234e5ba685af15f719b131df57f
Sincan2 <hadi@the-netwerk.com>
2022-11-20 01:57:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,12 +27,12 @@ mkdir /var/lib/crot;
 echo "IP=" >> /var/lib/crot/ipvps.conf
 cd
 #install tools/alat
-wget https://raw.githubusercontent.com/justnotiyann/V2ray-X2ray-443-Fork/main/install-tools.sh && chmod +x install-tools.sh && ./install-tools.sh
+wget https://raw.githubusercontent.com/Sincan2/V2ray-X2ray-443-Fork/main/install-tools.sh && chmod +x install-tools.sh && ./install-tools.sh
 #
 #Instal Xray
-wget https://raw.githubusercontent.com/justnotiyann/V2ray-X2ray-443-Fork/main/install-xray.sh && chmod +x install-xray.sh && ./install-xray.sh
+wget https://raw.githubusercontent.com/Sincan2/V2ray-X2ray-443-Fork/main/install-xray.sh && chmod +x install-xray.sh && ./install-xray.sh
 #install xmenu
-wget https://raw.githubusercontent.com/justnotiyann/V2ray-X2ray-443-Fork/main/menu/updatedll.sh && chmod +x updatedll.sh && ./updatedll.sh
+wget https://raw.githubusercontent.com/Sincan2/V2ray-X2ray-443-Fork/main/menu/updatedll.sh && chmod +x updatedll.sh && ./updatedll.sh
 #
 #SELESAI
 echo " "
